00bba028b81ea708ca18715738d69e782b3d62ab
Peterson <petnogueira@gmail.com>
2016-11-02 14:31:35 +0000

    Add test for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
+  root 'static_page#home'
+  
   get 'static_page/home'
   get 'static_page/help'
   get 'static_page/about'
   get 'static_page/contact'
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_page#home'
 end
